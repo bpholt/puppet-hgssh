4eeccd6b601609e1a590ba85b31c024c01a66f02
Brian P. Holt <bholt@planetholt.com>
2013-03-23 17:35:49 -0500

    augment documentation and format according to puppet style guide

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,8 @@
 # == Class: hgssh
 #
 # Defines a type to manage SSH keys for users accessing Mercurial
-# repositories via SSH
+# repositories via SSH. Including the base class will ensure
+# Mercurial is installed and an `hg` user exists.
 #
 # === Authors
 #
--- manifests/user.pp
+++ manifests/user.pp
@@ -1,10 +1,46 @@
-# User type
-# Parameters:
-#   name:         User's name
-#   email:        User's email
-#   repositories: paths to allowed repositories
-#   key:          SSH key
-#   type:         ssh-rsa or ssh-dsa
+# ## Define: user
+#
+# Defines a user that can access a specific set of hg repositories.
+#
+# ### Parameters
+#
+# [*name*]
+#   User's name. Will be added to the key comment.
+#
+# [*repositories*]
+#   Path(s) to repositories accessible to the user. Will be added
+#   to the `command` part of the key entry. Uses the same rules
+#   as hg-ssh.
+#
+# [*key*]
+#   The public key itself; generally a long string of hex characters. The
+#   key attribute may not contain whitespace: Omit key headers (e.g. 'ssh-rsa')
+#   and key identifiers (e.g. 'joe@joescomputer.local') found in the public
+#   key file.
+#
+# [*email*]
+#   (Optional) User's email. If set, will be added to the key comment.
+#
+# [*type*]
+#   (Optional) `ssh-rsa` or `ssh-dsa`
+#
+# ### Examples
+#
+#     hgssh::user { 'user1':
+#       name         => 'Some User',
+#       repositories => 'group/repo1 group/repo2',
+#       key          => 'keybytes',
+#       email        => 'someuser@example.com',
+#     }
+#
+# ### Authors
+#
+# Brian P. Holt <bholt@planetholt.com>
+#
+# ### Copyright
+#
+# Copyright 2013 Brian P. Holt
+#
 define hgssh::user (
   $name,
   $repositories,
